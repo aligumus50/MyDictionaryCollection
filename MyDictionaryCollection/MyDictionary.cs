358c755c15ae912ff9f1983c5d1bbb85ca389b70
aligumus50 <ali_gumus-50@hotmail.com>
2021-05-20 16:53:58 +0300

    Add project files.

---
+++ MyDictionaryCollection/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryCollection
+{
+    class MyDictionary<K, V>
+    {
+        K[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] tempArrayK = keys;
+            V[] tempArrayV = values;
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempArrayK.Length; i++)
+            {
+                keys[i] = tempArrayK[i];
+            }
+
+            for (int j = 0; j < tempArrayV.Length; j++)
+            {
+                values[j] = tempArrayV[j];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+    }
+}
---
+++ MyDictionaryCollection/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            //eng-tr
+            dictionary.Add("book", "kitap");
+            dictionary.Add("table", "tablo");
+            dictionary.Add("pencil", "kalem");
+
+            //Console.WriteLine(dictionary.Count);
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item.Value);
+            }
+
+            Console.WriteLine("***********************************");
+
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+
+            //eng-tr
+            myDictionary.Add("book", "kitap");
+            myDictionary.Add("table", "tablo");
+            myDictionary.Add("pencil", "kalem");
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item.Value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
